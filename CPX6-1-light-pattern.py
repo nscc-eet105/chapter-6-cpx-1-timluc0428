8b5c110640eb861bfa0167181e9893ca5b4bea72
timluc0428 <timluc0428@yahoo.com>
2025-06-20 08:28:55 -0400

    Update CPX6-1-light-pattern.py
    working update

--- CPX6-1-light-pattern.py
+++ CPX6-1-light-pattern.py
@@ -1,33 +1,44 @@
-from adafruit_circuitplayground import cp
+# from adafruit_circuitplayground import cp
 import time
+import random
 
 []#use a list to contain the neopixel light pins numbers in a specific repeating
 #pattern. The randint function is also used to create the actual light displayed.
-MAX_PiXEL = 9
-
+MAX_PIXEL = 9
+BLACK = (0, 0, 0)
 pixel_range = []
 
-
-for i in range(MAX_PiXEL + 1):
+#makes list of pixels
+for i in range(MAX_PIXEL + 1):
     pixel_range.append(int(i))
 
-print(pixel_range)
-
-
-#a list of the pins which use it pixel at least once.
+#makes random list of pixels
+pixel_pattern = random.sample(pixel_range, k=10)
 
 #a function that returns a tuple with the red, green and blue values randomly generated.
 #example
 def pixel_color():
     red = random.randint(0,255)
+    green = random.randint(0,255)
+    blue = random.randint(0,255)
+    return (red, green, blue)
+
+def pixel_black():
+    for i in range(MAX_PIXEL + 1)
+        cp.pixels[i] = BLACK
 
 #inside the infinite while loop use a for loop to iterate through the pattern list
-while True:
-    for pixel in pattern:
-        cp.pixels[pixel] = pixel_color()
-
+#while True:
+for pixel in pixel_pattern:
+#        pixel_black()
+#        time.sleep(.05)
+#        cp.pixels[pixel] = pixel_color()
+#        time.sleep(.05)
+    pix_color = pixel_color()
+    print(pixel, pix_color)
 #Inside the for loop the specific pixel should be turned on using the randomly
     #generated color.
     #Make sure this is a flash action, so it should then be turned off by setting 
     #the pixel to black as in previous labs
+
 
